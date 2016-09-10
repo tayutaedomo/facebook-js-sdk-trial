650cb447c06017a83ae4252c3d3ddabe34592fc1
tayutaedomo <tayutaedomo@gmail.com>
2016-09-10 14:51:58 +0900

    Add layout template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var title = 'Facebook Javascript SDK Trial';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: title });
 });
 
+router.get('/hello', function(req, res, next) {
+  //res.render('hello', { title : 'Hello | ' + title });
+  res.send('hello');
+});
+
+
 module.exports = router;
+
